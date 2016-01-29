be7e5c09e2b168e60dc6383a4567fc3584f13f43
Администратор <mr.kamc@gmail.com>
2016-01-29 15:24:40 +0300

    + метод execute класса Db

--- App/Db.php
+++ App/Db.php
@@ -7,9 +7,18 @@ namespace App;
 class Db
 {
 
+    protected $dbh;
+
     function __construct()
     {
-        $dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', 'werq');
+        $this->dbh = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '131318');
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->dbh->prepare($sql);
+        $res = $sth->execute();
+        return $res;
     }
 
 }
