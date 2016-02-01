5549c933926f3918d56e364f0ddfa23a543dff08
Администратор <mr.kamc@gmail.com>
2016-02-01 15:15:26 +0300

    добавлены методы обновления и удаления +метод save

--- App/Model.php
+++ App/Model.php
@@ -23,15 +23,14 @@ abstract class Model
         return $db->query(
             'SELECT * FROM ' . static::TABLE . ' WHERE id = :id',
             static::class, [':id' => $id]
-        )[0]?:false;
+        )[0] ?: false;
     }
 
-    public static  function findLastNews($num)
+    public static function findLastNews($num)
     {
         $db = Db::instance();
         return $db->query('SELECT * FROM ' . static::TABLE . ' ORDER BY id DESC LIMIT ' . $num,
             static::class);
-
     }
 
     public function isNew()
@@ -41,24 +40,66 @@ abstract class Model
 
     public function insert()
     {
-        if (!$this->isNew()){
+        if (!$this->isNew()) {
             return;
         }
 
         $columns = [];
         $values = [];
 
-        foreach($this as $k => $v){
-            if('id' == $k){
+        foreach ($this as $k => $v) {
+            if ('id' == $k) {
                 continue;
             }
             $columns[] = $k;
-            $values[':'.$k] = $v;
+            $values[':' . $k] = $v;
         }
 
-        $sql = 'INSERT INTO ' .static::TABLE . ' (' . implode(',',$columns) . ')
-         VALUES ('. implode(',',array_keys($values)).')';
+        $sql = 'INSERT INTO ' . static::TABLE . ' (' . implode(',', $columns) . ')
+         VALUES (' . implode(',', array_keys($values)) . ')';
         $db = Db::instance();
         $db->execute($sql, $values);
+    }
+
+    public function update()
+    {
+        if($this->isNew()) {
+            return;
+        }
+
+        $columns = [];
+        $values = [];
+
+        foreach ($this as $k => $v) {
+            if ('id' == $k) {
+                continue;
+            }
+            $columns[] = $k . '=:' . $k;
+            $values[':' . $k] = $v;
+        }
+
+        $sql = 'UPDATE ' . static::TABLE . ' SET ' . implode(',', $columns) . ' WHERE ' . 'id='.$this->id;
+        $db = Db::instance();
+        $db->execute($sql, $values);
+    }
+
+    public function delete()
+    {
+        if($this->isNew()) {
+            return;
+        }
+        $sql = 'DELETE FROM ' . static::TABLE . ' WHERE id=' . $this->id;
+        $db = Db::instance();
+        $db->execute($sql);
+    }
+
+    public function save()
+    {
+        if ($this->isNew()){
+            $this->insert();
+        }
+        else {
+            $this->update();
+        }
     }
 }
--- index.php
+++ index.php
@@ -7,7 +7,11 @@ require __DIR__ . '/autoload.php';
 $news = \App\Models\News::findLastNews(3);
 
 require __DIR__ . '/Views/news.php';*/
+
+
+
 $user = new User();
-$user->name = 'Vasya';
+$user->name = 'Vasilisa';
 $user->email = 'v@pupkin.ru';
 $user->insert();
+
