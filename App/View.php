3fa00d31e1e003d1a2e8dee2197d6184b8dfd763
alex <steam-punker@mail.ru>
2016-02-07 21:11:02 +0300

    +trait Magic

--- App/View.php
+++ App/View.php
@@ -6,18 +6,9 @@ namespace App;
 class View
     implements \Countable
 {
+    use Magic;
 
-    protected $data = [];
 
-    public function __set($name, $value)
-    {
-        $this->data[$name] = $value;
-    }
-
-    public function __get($name)
-    {
-        return $this->data[$name];
-    }
 
     public function display($path)
     {
